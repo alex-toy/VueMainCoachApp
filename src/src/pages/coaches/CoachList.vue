64c1af33f2b8fe26b61f3b1f332b515872039963
alex-toy <alexei.80@hotmail.fr>
2024-01-07 11:15:44 +0100

    base components

--- src/src/pages/coaches/CoachList.vue
+++ src/src/pages/coaches/CoachList.vue
@@ -1,11 +1,44 @@
 <template>
-    <div>
-        <section>Filter</section>
-        <section>
-            <div class="controls">
-                <button>Refresh</button>
-                <router-link to="/register">Register as a coach</router-link>
-            </div>
-        </section>
-    </div>
+    <section>Filter</section>
+    <section>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as a coach</router-link>
+        </div>
+        <ul v-if="hasCoaches">
+            <CoachItem v-for="coach in filteredCoaches" :key="coach.id" :coach="coach"></CoachItem>
+        </ul>
+        <h3 v-else>No coaches</h3>
+    </section>
 </template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue'
+
+export default {
+    components:{
+        CoachItem
+    },
+    computed:{
+        filteredCoaches(){
+            return this.$store.getters['coaches/coaches']
+        },
+        hasCoaches(){
+            return this.$store.getters['coaches/hasCoaches']
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
